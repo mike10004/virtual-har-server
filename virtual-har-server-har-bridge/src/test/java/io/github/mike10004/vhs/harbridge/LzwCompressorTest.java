64494cfb271f36320f405cdef04b8f665beea890
Mike Chaberski <mchaberski@gmail.com>
2018-02-23 13:45:36 -0500

    remove gpl lzw implementation and replace with lizard MIT-licensed implementation

--- virtual-har-server-har-bridge/src/test/java/io/github/mike10004/vhs/harbridge/LzwCompressorTest.java
+++ virtual-har-server-har-bridge/src/test/java/io/github/mike10004/vhs/harbridge/LzwCompressorTest.java
@@ -2,30 +2,18 @@ package io.github.mike10004.vhs.harbridge;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class LzwTest {
+class LzwCompressorTest {
 
     @Test
     void compressAndDecompress() throws Exception {
         String data = "This is a low-entropy string. This is a low-entropy string. This is a low-entropy string. This is a low-entropy string.";
-        ByteArrayOutputStream buffer1 = new ByteArrayOutputStream(data.length() + 16);
         byte[] inbytes = data.getBytes();
         System.out.format("%d input bytes%n", inbytes.length);
-        try (InputStream bin = new ByteArrayInputStream(inbytes)) {
-            new Lzw.Codec().compress(bin, buffer1);
-        }
-        byte[] compressedBytes = buffer1.toByteArray();
+        byte[] compressedBytes = new LzwCompressor().compress(inbytes);
         System.out.format("%d compressed bytes%n", compressedBytes.length);
-        ByteArrayOutputStream buffer2 = new ByteArrayOutputStream(data.length() + 16);
-        try (InputStream bin = new ByteArrayInputStream(compressedBytes)) {
-            new Lzw.Codec().decompress(bin, buffer2);
-        }
-        byte[] decompressedBytes = buffer2.toByteArray();
+        byte[] decompressedBytes = new LzwCompressor().decompress(compressedBytes);
         System.out.format("%d decompressed bytes%n", decompressedBytes.length);
         String actual = new String(decompressedBytes);
         assertEquals(data, actual, "string");
