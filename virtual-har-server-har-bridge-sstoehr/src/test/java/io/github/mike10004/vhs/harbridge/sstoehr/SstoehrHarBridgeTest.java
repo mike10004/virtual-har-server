fb59117826ec3b648382438d75ce5b729593fa8b
Mike Chaberski <mchaberski@gmail.com>
2018-02-26 18:30:36 -0500

    add unit tests showing behavior where Accept-Encoding is ignored

--- virtual-har-server-har-bridge-sstoehr/src/test/java/io/github/mike10004/vhs/harbridge/sstoehr/SstoehrHarBridgeTest.java
+++ virtual-har-server-har-bridge-sstoehr/src/test/java/io/github/mike10004/vhs/harbridge/sstoehr/SstoehrHarBridgeTest.java
@@ -1,73 +1,81 @@
 package io.github.mike10004.vhs.harbridge.sstoehr;
 
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.net.MediaType;
 import de.sstoehr.harreader.model.*;
 import io.github.mike10004.vhs.harbridge.HarBridge;
 
+import io.github.mike10004.vhs.harbridge.HarBridge.ResponseData;
+import io.github.mike10004.vhs.harbridge.ParsedRequest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-@org.junit.jupiter.api.Disabled
 class SstoehrHarBridgeTest {
 
     private HarBridge<HarEntry> bridge = new SstoehrHarBridge();
     private HarEntry entry;
-    private Multimap<String, String> requestHeaders, responseHeaders;
+    private URI url = URI.create("https://www.example.com/");
+    private MediaType contentType = MediaType.PLAIN_TEXT_UTF_8;
+    private String responseText = "hello";
+    @SuppressWarnings("ConstantConditions")
+    private byte[] responseBody = responseText.getBytes(contentType.charset().get());
 
     @BeforeEach
     private void thing() throws Exception {
         entry = new HarEntry();
         HarRequest request = new HarRequest();
         request.setMethod(HttpMethod.GET);
-        request.setUrl("https://www.example.com/");
+        request.setUrl(url.toString());
         HarResponse response = new HarResponse();
-        MediaType contentType = MediaType.PLAIN_TEXT_UTF_8;
-        //noinspection ConstantConditions
-        byte[] responseBody = "hello".getBytes(contentType.charset().get());
+        response.setStatus(200);
+        response.setStatusText("OK");
+        HarContent harContent = new HarContent();
+        harContent.setMimeType(contentType.toString());
+        harContent.setText(responseText);
         response.setBodySize((long) responseBody.length);
-        responseHeaders = ArrayListMultimap.create();
-//        response.setHeaders();
-    }
-
-    @Test
-    void getResponseData() throws Exception {
-        bridge.getResponseData(null, entry);
+        response.setContent(harContent);
+        entry.setRequest(request);
+        entry.setResponse(response);
     }
 
     @Test
     void getRequestMethod() throws Exception {
-        
+        assertEquals(HttpMethod.GET.name(), bridge.getRequestMethod(entry), "method");
     }
 
     @Test
     void getRequestUrl() throws Exception {
+        assertEquals(url.toString(), bridge.getRequestUrl(entry), "url");
     }
 
     @Test
     void getRequestHeaders() throws Exception {
+        assertEquals(0L, bridge.getRequestHeaders(entry).count(), "request header count");
     }
 
     @Test
-    void getResponseHeaders() throws Exception {
+    void getResponseData() throws Exception {
+        ParsedRequest request = ParsedRequest.inMemory(io.github.mike10004.vhs.harbridge.HttpMethod.GET, URI.create("http://www.example.com/"), ImmutableMultimap.of(), ImmutableMultimap.of(), null);
+        ResponseData responseData = bridge.getResponseData(request, entry);
+        assertEquals(responseBody.length, responseData.body.size(), "body size");
+        assertEquals(contentType, responseData.contentType);
+        assertEquals(0, responseData.headers.count());
     }
 
     @Test
     void getRequestPostData() throws Exception {
-    }
-
-    @Test
-    void getResponseBody() throws Exception {
-    }
-
-    @Test
-    void getResponseContentType() throws Exception {
+        assertNull(bridge.getRequestPostData(entry));
     }
 
     @Test
     void getResponseStatus() throws Exception {
+        assertEquals(200, bridge.getResponseStatus(entry));
     }
 }
